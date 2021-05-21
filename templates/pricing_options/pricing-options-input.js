198e96ee4ba01166577f4ae10cf09c6930ea21fa
Rob Whelan <whelan@gmail.com>
2021-05-21 10:42:36 -0500

    first version of pricing options

---
+++ templates/pricing_options/pricing-options-input.js
@@ -0,0 +1,18 @@
+const pricingOptionsInput = [{
+    'name': 'Option A',
+    'cost': 300,
+    'discount': '10%'
+    },
+    {
+        'name': 'Option B',
+        'cost': 300,
+        'discount': '10%'
+
+    },
+    {
+        'name': 'Option C',
+        'cost': 300,
+        'duration': '20 days',
+        'discount': '10%'
+
+    }];
